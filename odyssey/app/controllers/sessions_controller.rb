c581fb95f686bb04e099add0d107e64f47a5610b
Chase Smith <cnsmith@noctrl.edu>
2016-04-29 18:01:01 +0000

    Sessions controller now half-authenticates logins
    If the user ID is valid, then the Sessions controller redirects to /home/home1.
    Otherwise, it keeps the user on the login page.

--- odyssey/app/controllers/sessions_controller.rb
+++ odyssey/app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    #render 'pages/home/home1'
-    redirect_to '/home/home1'
+    # Find the user by their user ID
+    user = User.find_by(user_id: params[:session][:user_id])
+    # TODO: Will need to actually authenticate the user, once the Users
+    #  controller is finished.
+    #if user && user.authenticate(params[:session][:password])
+    
+    # If the user exists, send them to the home1 page
+    if user
+      redirect_to '/home/home1'
+    else
+      # Otherwise, keep them on the login page.
+      # TODO: Figure out why the flash message doesn't work
+      flash.now[:danger] = 'Invalid user ID/password combination'
+      render 'new'
+    end
   end
 end
