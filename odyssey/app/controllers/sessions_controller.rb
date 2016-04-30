8c8258991dd49c976a9d9bd2ee3acba16c9d1093
Chase Smith <cnsmith@noctrl.edu>
2016-04-30 03:53:11 +0000

    Logging in and out now works
    * Logging in will store the user_id in the session
    * Logging out will destroy the session
    * If `before_action :logged_in` is added to a controller, it will require the user be logged in before any action will be done.
    * `logged_in` will redirect the user to the login page, with a message saying "You are not logged in", if that is the case
    * The logout link in the navbar is now correct
    * Logging out will flash a (green) message saying "You have successfully been logged out."
    * Logging out is only allowed if the user is logged in

--- odyssey/app/controllers/sessions_controller.rb
+++ odyssey/app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
+  # Logging out is only allowed if the user is actually logged in
+  before_action :logged_in, only: [:destroy]
+  
   def new
   end
+  
+  # Create will log the user in, or display a message if the login attempt
+  #  is unsuccessful.
   def create
     # Find the user by their user ID
     user = User.find_by(user_id: params[:session][:user_id])
@@ -18,5 +24,16 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid user ID/password combination'
       render 'new'
     end
+  end
+  
+  # This is the log-out function. Will log the user out of the session,
+  #  and send them to the homepage.
+  def destroy
+    # Log the user out of the session
+    log_out
+    # Display the following message when the user has been redirected
+    flash[:success] = "You have successfully been logged out."
+    # Redirect the user to the login page, thus completing the logout process
+    redirect_to '/login'
   end
 end
