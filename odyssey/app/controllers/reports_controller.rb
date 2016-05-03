b6df2adadde4c8cf8d0b0e12eef1bbefb998bb0a
Gerardo A Paleo <gerardopaleo@gmail.com>
2016-05-03 02:50:35 +0000

    Can now download donor report. Began working on truck report form.

--- odyssey/app/controllers/reports_controller.rb
+++ odyssey/app/controllers/reports_controller.rb
@@ -6,15 +6,12 @@ class ReportsController < ApplicationController
           donors = Pickup.joins(:day).where("cast(strftime('%m', date) as int) = ?",
           params[:date][:month]).where("cast(strftime('%Y', date) as int) = ?",
           params[:date][:year])
-          
-          #filename = "#{params[:filename]}.csv"
-          #send_data donors.to_donor_csv, filename: filename}
-          
           send_data donors.to_donor_csv, filename: params[:filename]
         }
       end
   end
 
   def truck
+    @days = Day.all
   end
 end
