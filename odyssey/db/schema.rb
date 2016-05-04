44331f49b7a37fd52b14917cff6a7fd14c366455
charlie_miller <cjmiller@noctrl.edu>
2016-05-04 17:29:58 +0000

    Added authentication stuff

--- odyssey/db/schema.rb
+++ odyssey/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160503020416) do
-=======
 ActiveRecord::Schema.define(version: 20160503020705) do
->>>>>>> upstream/master
 
   create_table "days", force: :cascade do |t|
     t.date     "date"
