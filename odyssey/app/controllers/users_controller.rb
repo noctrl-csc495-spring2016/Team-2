2a46d1af356734a6e5fa23ebe6b935b73d053c25
charlie_miller <cjmiller@noctrl.edu>
2016-05-04 00:50:55 +0000

    added something

--- odyssey/app/controllers/users_controller.rb
+++ odyssey/app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     
     def edit
         @user = User.find(params[:id])
+        redirect_to action: "update"
     end
     
     def new
@@ -24,20 +25,18 @@ class UsersController < ApplicationController
         redirect_to users_url
     end
     
-    
     def update
         @user = User.find(params[:id])
         if @user.update_attributes(user_params)
           flash[:success] = "Profile updated"
           redirect_to users_url
         else
-          render 'edit'
+          redirect_to action: "edit"
         end    
     end
+    
     def create
         @user = User.new(user_params)
-        
-            
         if @user.save
           redirect_to action: "index"
         else
@@ -48,6 +47,6 @@ class UsersController < ApplicationController
     private
     
     def user_params
-      params.require(:user).permit(:user_name, :user_email, :permission_level)
+      params.require(:user).permit(:user_name, :user_email, :permission_level, :password, :password_confirmation)
     end
 end
--- odyssey/db/schema.rb
+++ odyssey/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425175427) do
+ActiveRecord::Schema.define(version: 20160503020416) do
 
   create_table "days", force: :cascade do |t|
     t.date     "date"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160425175427) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "status"
+    t.string   "password_digest"
   end
 
 end
