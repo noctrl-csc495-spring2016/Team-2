e95763e717df563d0e7305a66aecec68ca2fbfc6
Gerardo A Paleo <gerardopaleo@gmail.com>
2016-05-09 02:20:32 +0000

    Very simple tests...rip rip little man.

--- odyssey/test/controllers/reports_controller_test.rb
+++ odyssey/test/controllers/reports_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should donor report form" do
+    log_in_as(users(:bill))
+    get :donor
+    assert_response :success
+  end
+  
+  test "should get truck report form" do
+    log_in_as(users(:bill))
+    get :truck
+    assert_response :success
+  end
+  
+  test "should get main reports page" do
+    log_in_as(users(:bill))
+    get :reports
+    assert_response :success
+  end
 end
